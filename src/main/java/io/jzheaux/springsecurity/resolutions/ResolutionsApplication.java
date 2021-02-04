05e64767a69521cde92030f1e640722c4038d3e7
Donka <dltaneva@gmail.com>
2021-02-04 17:56:08 +0200

    Initial first module commit

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -3,12 +3,25 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication //(exclude = SecurityAutoConfiguration.class)
 public class ResolutionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResolutionsApplication.class, args);
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new InMemoryUserDetailsManager(
+				org.springframework.security.core.userdetails.User
+						.withUsername("user")
+						.password("{bcrypt}$2a$10$SATdMR3qqfvylTVP0os.LODwc0bAiFN32X3sN01UM5vAA2/WVdZ0m")
+						.authorities("resolution:read")
+						.build());
 	}
 
 }
