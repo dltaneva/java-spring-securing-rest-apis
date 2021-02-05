85884d16821d900ea298b005fdaca01a7de9b5a4
Donka <dltaneva@gmail.com>
2021-02-05 17:45:48 +0200

    module 1-12

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -5,6 +5,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserRepositoryUserDetailsService implements UserDetailsService {
+
+    private final UserRepository users;
+
+    public UserRepositoryUserDetailsService(UserRepository users) {
+        this.users = users;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         throw new UsernameNotFoundException("no user");
